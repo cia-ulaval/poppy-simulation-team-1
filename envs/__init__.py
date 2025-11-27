88b9f57a3238482809a5059de57169ef6b59fd74
bat0001 <baptiste.bonin10@gmail.com>
2025-11-27 12:56:42 -0500

    remove legacy code

--- envs/__init__.py
+++
@@ -1,9 +0,0 @@
-
-# Module des environnements pour Poppy RL
-
-from .mujoco_humanoid import MuJoCoHumanoidEnv, make_humanoid_env
-
-__all__ = [
-    'MuJoCoHumanoidEnv',
-    'make_humanoid_env',
-]
