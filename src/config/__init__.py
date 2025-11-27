e5ecdfe0d8d8c5d95dcd81e16bdb6754f0a08099
bat0001 <baptiste.bonin10@gmail.com>
2025-11-27 12:26:47 -0500

    add config folder

---
+++ src/config/__init__.py
@@ -0,0 +1,23 @@
+from src.config.settings import (
+    AlgorithmType,
+    NetworkConfig,
+    EnvironmentConfig,
+    TrainingConfig,
+    PPOConfig,
+    TD3Config,
+    SACConfig,
+    A2CConfig,
+    ExperimentConfig,
+)
+
+__all__ = [
+    "AlgorithmType",
+    "NetworkConfig",
+    "EnvironmentConfig",
+    "TrainingConfig",
+    "PPOConfig",
+    "TD3Config",
+    "SACConfig",
+    "A2CConfig",
+    "ExperimentConfig",
+]
