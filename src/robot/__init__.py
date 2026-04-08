768c815ee5720094cfa2687e869eaa129cd47c63
Léo <leopold@lchappuis.fr>
2026-04-08 13:30:52 -0400

    feat(pypot): integrate pypot loop sim2real

---
+++ src/robot/__init__.py
@@ -0,0 +1,10 @@
+from .robot_controller import RobotController
+from .robot_config import RobotConfig, POPPY_HUMANOID_CONFIG
+from .pypot_adapter import PypotAdapter
+
+__all__ = [
+    "RobotController",
+    "RobotConfig",
+    "POPPY_HUMANOID_CONFIG",
+    "PypotAdapter",
+]
